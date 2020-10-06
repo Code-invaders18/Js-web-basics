ba2effd978331c4df608548aa446167fc42dd5d1
Vansh Malik <52025723+Code-invaders18@users.noreply.github.com>
2020-10-06 10:46:07 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,12 @@
+// // //alert('A FILE IS ATTACHED')
+
+// // const change=document.querySelectorAll('p')
+
+// // change.foreach(function(p){
+// //     p.remove();
+// // })
+// //document.title='i got changed'
+
+// console.log(document.getElementById('idone'));
+const myelement=document.querySelectorAll('p')
+console.log(myelement);
